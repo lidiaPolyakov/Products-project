58916b68fbfef8ad1cb1aab513dc5ac56cfa11e4
Asaf Baibekov <asafall21@gmail.com>
2024-04-06 18:00:34 +0300

    in main seperate ds2 and ds4 flows

--- Alpha/main.py
+++ Alpha/main.py
@@ -3,37 +3,20 @@ import os
 import json
 from DataInputer import DataInputer
 from KNNDataProcessor import KNNDataProcessor
+
 from DS2.DS2PreProcessor import DS2PreProcessor
+from DS2.DS2XGBoostPredictor import DS2XGBoostPredictor
+
 from DS4.DS4PreProcessor import DS4PreProcessor
 from DS4.DS4NaiveBayesPredictor import DS4NaiveBayesPredictor
 
-def main():
 
-    with open('./Alpha/common_columns.json') as f:
-        common_columns = json.load(f)
-
-    data_processor = DataInputer(common_columns)
-    mock_data = data_processor.get_mock_data()
-    query_ds2, query_ds4 = data_processor.prepare_queries(mock_data)
-
-    df2 = pd.read_csv('./Alpha/datasets/dataset2.csv')
-    ds2_preprocessor = DS2PreProcessor(df2)
-    df2 = ds2_preprocessor.preprocessed_df2
-    df2.name = 'ds2'
-
+def ds4(common_columns, query_ds4):
     df4 = pd.read_csv('./Alpha/datasets/dataset4.csv')
     ds4_preprocessor = DS4PreProcessor(df4)
     df4 = ds4_preprocessor.preprocessed_df4
     df4.name = 'ds4'
-
-    knn_data_processor_ds2 = KNNDataProcessor(common_columns, df2)
-    user_input_processed_df2 = knn_data_processor_ds2.prepare_user_input_for_knn(query_ds2, "ds2")
-    nearest_neighbor_row_ds2 = knn_data_processor_ds2.find_nearest_neighbor(user_input_processed_df2)
-    print(f"Query for dataset2: {query_ds2}")
-    print("Nearest neighbor in dataset2:")
-    print(nearest_neighbor_row_ds2)
-    print()
-
+    
     knn_data_processor_ds4 = KNNDataProcessor(common_columns, df4)
     user_input_processed_df4 = knn_data_processor_ds4.prepare_user_input_for_knn(query_ds4, "ds4")
     nearest_neighbor_row_ds4 = knn_data_processor_ds4.find_nearest_neighbor(user_input_processed_df4)
@@ -41,7 +24,6 @@ def main():
     print("Nearest neighbor in dataset4:")
     print(nearest_neighbor_row_ds4)
     print()
-
     
     print("Predicting hospital_death using Naive Bayes on dataset4")
     predictor = DS4NaiveBayesPredictor(df4)
@@ -51,6 +33,31 @@ def main():
 
     prediction = predictor.predict(nearest_neighbor_row_ds4, path)
     print(f"Prediction: {prediction}")
+
+def ds2(common_columns, query_ds2):
+    df2 = pd.read_csv('./Alpha/datasets/dataset2.csv')
+    ds2_preprocessor = DS2PreProcessor(df2)
+    df2 = ds2_preprocessor.preprocessed_df2
+    df2.name = 'ds2'
+
+    knn_data_processor_ds2 = KNNDataProcessor(common_columns, df2)
+    user_input_processed_df2 = knn_data_processor_ds2.prepare_user_input_for_knn(query_ds2, "ds2")
+    nearest_neighbor_row_ds2 = knn_data_processor_ds2.find_nearest_neighbor(user_input_processed_df2)
+    print(f"Query for dataset2: {query_ds2}")
+    print("Nearest neighbor in dataset2:")
+    print(nearest_neighbor_row_ds2)
+    print()
+
+def main():
+    with open('./Alpha/common_columns.json') as f:
+        common_columns = json.load(f)
+    data_processor = DataInputer(common_columns)
+    mock_data = data_processor.get_mock_data()
+    query_ds2, query_ds4 = data_processor.prepare_queries(mock_data)
+    
+    ds2(common_columns, query_ds2)
+    
+    ds4(common_columns, query_ds4)
 
 if __name__ == '__main__':
     main()
