dcd9cb06cdc8a00acebc697bfb98537479d29732
Asaf Baibekov <asafall21@gmail.com>
2024-05-11 15:14:44 +0300

    nearest_neighbor_metric add complexity to categoric distance

--- Alpha/KNNDataProcessor.py
+++ Alpha/KNNDataProcessor.py
@@ -12,6 +12,10 @@ class KNNDataProcessor:
         self.label_encoders = {}
         self.scalers = {}
         self.min_max_scalers = np.array([])
+        self.min_frequancies = np.array([])
+        
+        self.max_freq_matrices = []
+        self.frequency_of_each_value = []
 
         for col_info in self.common_columns:
             datatype = col_info["datatype"]
@@ -23,6 +27,19 @@ class KNNDataProcessor:
                 df_copy[column_name] = le.fit_transform(df_copy[column_name].astype(str))
                 self.label_encoders[column_name] = le
                 self.min_max_scalers = np.append(self.min_max_scalers, np.nan)
+
+                self.min_frequancies = np.append(self.min_frequancies, df_copy[column_name].value_counts().min())
+
+                frequency = df_copy[column_name].dropna().value_counts()
+                unique_values = frequency.index
+
+                self.frequency_of_each_value.append(frequency)
+
+                max_freq_matrix = pd.DataFrame(index=unique_values, columns=unique_values)
+                for value1 in unique_values:
+                    for value2 in unique_values:
+                        max_freq_matrix.loc[value1, value2] = max(frequency[value1], frequency[value2])
+                self.max_freq_matrices.append(max_freq_matrix)
             else:
                 scaler = StandardScaler()
                 df_copy[column_name] = scaler.fit_transform(df_copy[[column_name]])
@@ -56,14 +73,28 @@ class KNNDataProcessor:
 
         return processed_input
 
-    def nearest_neighbor_metric(self, x, y, min_max_scalers):
-        categorical = np.isnan(min_max_scalers)
+    def nearest_neighbor_metric(self, x, y):
+        categorical = np.isnan(self.min_max_scalers)
         numeric = ~categorical
-        return np.sqrt(
-            np.sum(((x[numeric] - y[numeric]) / min_max_scalers[numeric]) ** 2)
-            +
-            np.sum(x[categorical] != y[categorical])
-        )
+        numeric_distance = np.sum(((x[numeric] - y[numeric]) / self.min_max_scalers[numeric]) ** 2)
+        
+        categorical_distance = 0
+        x_categorical = x[categorical]
+        y_categorical = y[categorical]
+        
+        for i in range(categorical.sum()):
+            if x_categorical[i] != y_categorical[i]:
+                x_category = x_categorical[i]
+                y_category = y_categorical[i]
+                max_freq = self.max_freq_matrices[i].loc[x_category, y_category]
+                min_freq_of_col = self.min_frequancies[i]
+                frequency_of_x_i = self.frequency_of_each_value[i][x_category]
+                frequency_of_y_i = self.frequency_of_each_value[i][y_category]
+                categorical_distance += (
+                    abs(frequency_of_x_i - frequency_of_y_i) + min_freq_of_col
+                ) / max_freq
+        
+        return (numeric_distance + categorical_distance) ** 0.5
 
     def inverse_transform_row(self, row):
         # inverse transform the row to get the original values
@@ -90,8 +121,7 @@ class KNNDataProcessor:
         
         df_processed_relevant = df_copy[intersection_columns]
 
-        metric = lambda x, y: self.nearest_neighbor_metric(x, y, self.min_max_scalers)
-        knn = NearestNeighbors(n_neighbors=1, metric=metric)
+        knn = NearestNeighbors(n_neighbors=1, algorithm='brute', metric=self.nearest_neighbor_metric)
         knn.fit(df_processed_relevant)
 
         knn_input_df = pd.DataFrame([user_input_processed], columns=intersection_columns)
